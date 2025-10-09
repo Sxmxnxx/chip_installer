d1faef3793aaf03913ceb397faa908a78f2967d8
Sumin Yang <9292sumin@gmail.com>
2025-10-10 00:27:25 +0900

    minor

---
+++ chip_release/src/App.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <!-- Title Section -->
+    <section id="title">
+      <div class="container">
+        <h1 id="logo"><a>CHIP Installer</a></h1>
+        <p>geo<strong>CH</strong>ronology <strong>I</strong>nteractive <strong>P</strong>ersonal database</p>
+      </div>
+    </section>
+
+    <!-- Main Section -->
+    <!-- <section id="main">
+      <div class="container">
+        <div class="main-left">
+          <img src="./assets/mainpic.jpg" alt="pic">
+        </div>
+        <div class="main-right">
+          <h2 class="main-title">IDFDFDFDFDF</h2>
+          <h3 class="main-sub">ㅇㄹㅇㄹㅇㄹㅇ</h3>
+          <p class="main-desc">ㄹㅇㄹㅇㄹㅇㄹㅇㄹ</p>
+          <ul class="actions">
+            <li>
+              <a class="button" :href="macLink" download>
+                <i class="fa-brands fa-apple"></i> macOS Installer
+              </a>
+            </li>
+            <li>
+              <a class="button" :href="winLink" download>
+                <i class="fa-brands fa-windows"></i> Windows Installer
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section> -->
+
+    <section id="main">
+      <div class="container">
+          <img src="./assets/mainpic.jpg" alt="pic">
+          <h2 class="main-title">IDFDFDFDFDF</h2>
+          <h3 class="main-sub">ㅇㄹㅇㄹㅇㄹㅇ</h3>
+          <p class="main-desc">ㄹㅇㄹㅇㄹㅇㄹㅇㄹ</p>
+          <ul class="actions">
+            <li>
+              <a class="button" :href="macLink" download>
+                <i class="fa-brands fa-apple"></i> macOS Installer
+              </a>
+            </li>
+            <li>
+              <a class="button" :href="winLink" download>
+                <i class="fa-brands fa-windows"></i> Windows Installer
+              </a>
+            </li>
+          </ul>
+      
+      </div>
+    </section>
+
+    <!-- Footer Section -->
+    <section id="footer">
+      <div class="container">
+        <header>
+          <h2>Questions or comments? <strong>Get in touch:</strong></h2>
+        </header>
+        <div class="row">
+          
+            <section>
+              <div class="row">              
+                  <ul class="icons">
+                    <li>
+                      <i class="fa-solid fa-envelope"></i>
+                      <a>chipproject.contact@gmail.com</a>
+                    </li>
+                    <li>
+                      <i class="fa-brands fa-github"></i>
+                      <a>배포본 깃 완성되면 추가</a>
+                    </li>
+                  </ul>
+              </div>
+            </section>
+          
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script setup>
+const macLink = '/downloads/CHIP-0.1.0-arm64.dmg.zip'
+// const winLink = '/downloads/chip-win.exe'
+</script>
+
+
+<style scoped>
+  #main .main-left {
+    flex: 0 0 40%;
+    aspect-ratio: 1 / 1;
+    overflow: hidden;
+  }
+
+  #main .main-left img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    display: block;
+  }
+
+  #main .main-right {
+    flex: 0 0 60%;
+    padding: 0 2rem;
+    flex-wrap: wrap;
+    text-align: right;
+  }
+
+</style>
